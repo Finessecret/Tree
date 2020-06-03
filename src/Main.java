43aac6e45e15e1dfa77c469d6144ee331e4dc5a3
Finessecret <66252254+Finessecret@users.noreply.github.com>
2020-06-03 19:23:08 +0300

    Поправка условий

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
         DefaultBinaryTree<Integer> tree = new BinaryTree<Integer>(s -> Integer.parseInt(s));
-        tree.fromBracketNotation("5 (4 , 7)");
+        tree.fromBracketNotation("8 (6 (4 (5), 6), 5 (, 5 (2, 8)))");
         tree.solution();
         System.out.print(tree.toBracketStr());
     }
